d4a64ae2b9ce0eafb88546da0bd8eb2f6973181d
unscodst <spikester@sbcglobal.net>
2015-12-03 19:57:48 -0500

    FactureUpdate
    Added Submit button that adds data after the last found <tr> tag

---
+++ Week 16/FactureDB/scripts.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+    $("#button").click(function() {
+        var logo = $('#logo').val();
+        var company = $('#company').val();
+        var country = $('#country').val();
+        var description = $('#description').val();
+        var materials = $('#materials').val();
+        var website = $('#website').val();
+        var email = $('#email').val();
+        var phone = $('#phone').val();
+    
+        $('#myTable tr:last').after("<tr><td>" + logo + "</td><td>" + company + "</td><td>" + country + "</td><td>" + description + "</td><td>" + materials + "</td><td>" + website + "</td><td>" + email + "</td><td>" + phone + "</td></tr>");
+
+    })
+});
