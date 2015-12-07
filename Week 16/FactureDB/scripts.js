7cb5b04e0c678a30df224762fad1b4c0debb0137
unscodst <spikester@sbcglobal.net>
2015-12-06 20:45:33 -0500

    Phone Validation
    Beginning of phone number validation. Currently only looks for a number
    not a pattern.

--- Week 16/FactureDB/scripts.js
+++ Week 16/FactureDB/scripts.js
@@ -8,8 +8,39 @@ $(document).ready(function() {
         var website = $('#website').val();
         var email = $('#email').val();
         var phone = $('#phone').val();
-    
-        $('#myTable tr:last').after('<tr><td> <img class="logoimg" src="'+ logo + '"></td><td>' + company + '</td><td>' + country + '</td><td>' + description + '</td><td>' + materials + '</td><td>' + website + '</td><td>' + email + '</td><td>' + phone + '</td></tr>');
 
-    })
+        $('#myTable tr:last').after('<tr><td> <img class="logoimg" src="' + logo + '"></td><td>' + company + '</td><td>' + country + '</td><td>' + description + '</td><td>' + materials + '</td><td>' + website + '</td><td>' + email + '</td><td>' + phone + '</td></tr>');
+
+    });
+
+
+
+
 });
+
+
+function validateInput() {
+    var number = $('#phone').val();
+    if(isNaN(number)) {
+        promptMessage("Numbers only", "phonePrompt", "red")
+        return false;
+    } else {
+        promptMessage("","phonePrompt","")
+        return true;
+    }
+}
+function promptMessage(message, promptLocation, color) {
+    document.getElementById(promptLocation).innerHTML = message;
+    document.getElementById(promptLocation).style.color = color;
+
+}
+
+
+
+
+
+
+
+
+
+
